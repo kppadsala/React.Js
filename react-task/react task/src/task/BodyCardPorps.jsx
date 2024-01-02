1cdabd5ce1da0e44a06726d935fc0d96ee90e186
krunal padsala <krunalpadsala30@gmail.com>
2024-01-02 10:40:33 +0530

    commit

--- react-task/react task/src/task/BodyCardPorps.jsx
+++ react-task/react task/src/task/BodyCardPorps.jsx
@@ -4,9 +4,9 @@ import Bodycard from './Bodycard'
 export default function BodyCardPorps() {
   return (
       <div className='d-flex justify-content-evenly'>
-          <Bodycard name={"Contained-3"} />
-          <Bodycard name={ "Contained-4"} />
-          <Bodycard name={ "Contained-5"} />
+          <Bodycard name={"Lorem ipsum dolor sit amet consectetur adipisicing elit."} title={"Image-1"} />
+          <Bodycard name={ " Quod accusamus quisquam, sequi commodi rerum blanditiis."} title={"Image-2"} />
+          <Bodycard name={ " ducimus similique at modi numquam facilis obcaecati eum ratione."} title={"Image-3"} />
           
     </div>
   )
--- react-task/react task/src/task/Bodycard.jsx
+++ react-task/react task/src/task/Bodycard.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from 'reactstrap'
 
 export default function Bodycard(props) {
   return (
-    <div className='h-100 w-100 p-3 m-5 border border-2 border-primary'>
-   <h3>it`s name is {props?.name}</h3>
-    </div>
+  //   <div className='h-100 w-100 p-3 m-5 border border-2 border-primary'>
+  //  <h3>it`s name is {props?.name}</h3>
+    //   </div>
+    <Card
+  color="dark"
+  inverse
+  style={{
+    width: '22rem',
+    margin:'25px 0px'
+    
+  }}
+>
+  <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"
+  />
+  <CardBody>
+    <CardTitle tag="h5">
+      {props.title}
+    </CardTitle>
+    <CardSubtitle
+      className="mb-2 text-muted"
+      tag="h6"
+    >
+      Card subtitle
+    </CardSubtitle>
+    <CardText>
+     {props.name}
+    </CardText>
+    <Button>
+      Button
+    </Button>
+  </CardBody>
+</Card>
 
     )
 }
